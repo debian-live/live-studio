748c05d93c38250458ab877eb9bb9d222cedacd0
Chris Lamb <lamby@debian.org>
2010-07-28 16:00:25 -0400

    Require login for all pages except ones we opt out of.s
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- config/settings/defaults/middleware.py
+++ config/settings/defaults/middleware.py
@@ -2,4 +2,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+    'live_studio_www.auth.middleware.RequireLoginMiddleware',
 )
