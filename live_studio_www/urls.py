f8dad3dfffb142aa1b6ad851355212c66156dd12
Chris Lamb <lamby@debian.org>
2010-07-28 16:54:28 -0400

    Rename 'builds' to 'config'.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio_www/urls.py
+++ live_studio_www/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'', include('live_studio_www.debug.urls', namespace='debug')),
-    (r'', include('live_studio_www.builds.urls', namespace='builds')),
+    (r'', include('live_studio_www.config.urls', namespace='config')),
     (r'', include('live_studio_www.static.urls', namespace='static')),
 )
