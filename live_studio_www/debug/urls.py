ca6785ed0463c4307c1e5aa38d4ea94a18346e49
Chris Lamb <lamby@debian.org>
2010-07-28 16:37:53 -0400

    Let Django serve static media if we are in DEBUG mode.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio_www/debug/urls.py
+++ live_studio_www/debug/urls.py
@@ -1,4 +1,13 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('live_studio_www.debug.views',
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'^media/(?P<path>.*)$', 'serve',
+            {'document_root': settings.MEDIA_ROOT}),
+        (r'^(?P<path>favicon.ico|robots\.txt)$', 'serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    )
