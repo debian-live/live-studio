24774ea44b0d7bd48a09f5f812f08fb3a18ec72f
Chris Lamb <lamby@debian.org>
2010-07-28 17:35:28 -0400

    Add stub view/edit config view.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio_www/config/urls.py
+++ live_studio_www/config/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('live_studio_www.config.views',
     url(r'configs$', 'configs', name='configs'),
+    url(r'config/(?P<config_id>\d+)$', 'config', name='config'),
 )
--- live_studio_www/config/views.py
+++ live_studio_www/config/views.py
@@ -1,4 +1,15 @@
+from django.shortcuts import get_object_or_404
+
 from live_studio_www.utils import render_response
+
+from .models import Config
 
 def configs(request):
     return render_response(request, 'config/configs.html')
+
+def config(request, config_id):
+    c = get_object_or_404(Config, pk=config_id, user=request.user)
+
+    return render_response(request, 'config/config.html', {
+        'config': c,
+    })
