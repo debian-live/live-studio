98a02cb08aa3a8a19992bc941bbddfe1c6a7c3c0
Chris Lamb <lamby@debian.org>
2010-07-29 11:09:51 -0400

    Expand queue_runner a bit more.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio_www/queue/management/commands/queue_runner.py
+++ live_studio_www/queue/management/commands/queue_runner.py
@@ -1,5 +1,21 @@
+import time
+
 from django.core.management.base import NoArgsCommand
+
+from live_studio_www.queue.models import Entry
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         self.verbose = int(options['verbosity']) > 1
+
+        while True:
+            try:
+                # Pop
+                entry = Entry.objects.all()[0]
+                self.handle_entry(entry)
+            except IndexError:
+                time.sleep(2)
+
+    def handle_entry(self, entry):
+        # Process 'entry' here
+        pass
