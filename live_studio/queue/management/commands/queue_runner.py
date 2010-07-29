37796d7a999cbac312f3eb611b3f714476583c67
Chris Lamb <lamby@debian.org>
2010-07-29 14:33:59 -0400

    Use logging over print statements.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio/queue/management/commands/queue_runner.py
+++ live_studio/queue/management/commands/queue_runner.py
@@ -1,5 +1,6 @@
 import time
 import shutil
+import logging
 import datetime
 import tempfile
 import traceback
@@ -10,31 +11,39 @@ from live_studio.queue.models import Entry
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        self.verbose = int(options['verbosity']) > 1
+        logging.basicConfig(level=logging.INFO)
+        self.log = logging.getLogger('live-studio-runner')
+
+        if int(options['verbosity']) > 1:
+            self.log.setLevel(logging.DEBUG)
 
         while True:
             try:
                 entry = Entry.objects.pop()
 
                 def update(**kwargs):
-                    print entry.pk, kwargs
+                    self.log.debug('Updating #%d with %r', entry.pk, kwargs)
                     Entry.objects.filter(pk=entry.pk).update(**kwargs)
 
                 update(started=datetime.datetime.utcnow())
-
                 tempdir = tempfile.mkdtemp(prefix='live-studio_')
+
+                self.log.info("Building #%d in %s", entry.pk, tempdir)
 
                 try:
                     self.handle_entry(entry, tempdir)
-                    update(success=True)
+                    update(finished=datetime.datetime.utcnow(), success=True)
+                    self.log.info("Entry #%d built successfully", entry.pk)
                 except:
-                    traceback.print_exc()
+                    update(finished=datetime.datetime.utcnow())
+                    self.log.exception("Entry #%d failed", entry.pk)
                     continue
                 finally:
-                    update(finished=datetime.datetime.utcnow())
                     shutil.rmtree(tempdir)
+                    self.log.info("Finished processing #%d", entry.pk)
 
             except IndexError:
+                self.log.debug('No items in queue, sleeping for 2s')
                 time.sleep(2)
 
     def handle_entry(self, entry, tempdir):
