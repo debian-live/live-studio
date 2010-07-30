0ba0285babaa74c7b5aa075d0c5a2e56a7a83273
Chris Lamb <lamby@debian.org>
2010-07-30 16:06:04 -0400

    Need this under pysupport for some reason.

--- live_studio/settings/role.py
+++ live_studio/settings/role.py
@@ -1,4 +1,4 @@
 # This file is overriden by the build system to load the 'live' or 'stage'
 # roles as needed. You should not add any custom configuration here.
 
-from roles.local import *
+from live_studio.settings.roles.local import *
