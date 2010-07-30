581fcdd53fbe5d1620166c7f47b4a25737141497
Chris Lamb <lamby@debian.org>
2010-07-30 14:20:14 -0400

    No build management command.

--- live_studio/settings/__init__.py
+++ live_studio/settings/__init__.py
@@ -4,8 +4,6 @@ from defaults import *
 
 if sys.argv[1:2] == ['test']:
     from roles.test import *
-elif sys.argv[1:2] == ['build']:
-    exec "from roles.%s import *" % sys.argv[2]
 else:
     from role import *
 
