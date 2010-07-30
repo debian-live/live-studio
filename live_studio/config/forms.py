106ea580471387a3645877f52018ff2880db34f3
Chris Lamb <lamby@debian.org>
2010-07-29 21:36:30 -0400

    Use radio buttons for most of the interface.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio/config/forms.py
+++ live_studio/config/forms.py
@@ -21,6 +21,13 @@ for fields in PAGES:
     meta = type('Meta', (), {
         'model': Config,
         'fields': fields,
+        'widgets': {
+            'base': forms.RadioSelect(),
+            'distribution': forms.RadioSelect(),
+            'media_type': forms.RadioSelect(),
+            'architecture': forms.RadioSelect(),
+            'installer': forms.RadioSelect(),
+        },
     })
 
     WIZARD_FORMS.append(type('', (forms.ModelForm,), {'Meta': meta}))
