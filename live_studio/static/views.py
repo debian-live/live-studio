975f4745b9123a299ec60ec4eed62028bbe68298
Chris Lamb <lamby@debian.org>
2010-07-29 22:09:08 -0400

    Welcome page doesn't make much sense if you are logged in.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- live_studio/static/views.py
+++ live_studio/static/views.py
@@ -1,6 +1,12 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
 from live_studio.utils import render_response
 from live_studio.auth.decorators import login_not_required
 
 @login_not_required
 def welcome(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
     return render_response(request, 'static/welcome.html', {})
